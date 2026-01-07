557041146ccd2e858ad425d7c289af6fab822e56
vikasthakurr <vikaskumar20012001@gmail.com>
2026-01-07 13:47:32 +0530

    notes pushed

--- javascript/conditional.js
+++ javascript/conditional.js
@@ -1,26 +1,123 @@
-// let age = 19;
-// if (age > 18) {
-//   console.log("you are able to vote");
-// } else if (age === "18") {
-//   console.log("you can vote but u are just 18");
-// } else {
-//   console.log("not able to vote");
-// }
+/*
+  ==============================================================
+  JAVASCRIPT CONDITIONAL STATEMENTS
+  ==============================================================
 
-// console.log(age > 18 ? "can vote" : "cant vote");
+  Conditional statements are used to perform different actions based 
+  on different conditions. They control the flow of execution in 
+  a program.
 
-// let marks=90;
+  --------------------------------------------------------------
+  1. IF ... ELSE IF ... ELSE
+  --------------------------------------------------------------
+  - The 'if' statement executes a block of code if a condition is true.
+  - 'else if' specifies a new condition to test, if the first condition is false.
+  - 'else' executes a block of code if all previous conditions are false.
 
-// switch (day) {
-//   case 90:
-//     console.log("+");
-//     break;
-//   case 2:
-//     console.log("tuesday");
-//     break;
-//   default:
-//     console.log("invalid day");
-// }
+  Syntax:
+  if (condition1) {
+    // code to be executed if condition1 is true
+  } else if (condition2) {
+    // code to be executed if condition1 is false and condition2 is true
+  } else {
+    // code to be executed if condition1 and condition2 are false
+  }
+*/
 
+let age = 19;
 
+if (age > 18) {
+  console.log("you are able to vote");
+} else if (age === "18") {
+  console.log("you can vote but u are just 18");
+} else {
+  console.log("not able to vote");
+}
 
+/*
+  --------------------------------------------------------------
+  2. TERNARY OPERATOR (Shorthand if-else)
+  --------------------------------------------------------------
+  - It is the only JavaScript operator that takes three operands.
+  - Often used as a shortcut for the 'if' statement.
+
+  Syntax:
+  condition ? expressionIfTrue : expressionIfFalse;
+*/
+
+console.log(age > 18 ? "can vote" : "cant vote");
+
+/*
+  --------------------------------------------------------------
+  3. SWITCH STATEMENT
+  --------------------------------------------------------------
+  - Used to perform different actions based on different conditions.
+  - It is often more efficient than long if-else if chains.
+  - 'break' prevents the code from running into the next case.
+  - 'default' specifies the code to run if there is no case match.
+
+  Syntax:
+  switch(expression) {
+    case x:
+      // code block
+      break;
+    case y:
+      // code block
+      break;
+    default:
+      // code block
+  }
+*/
+
+let marks = 90;
+
+switch (marks) {
+  case 90:
+    console.log("Grade: A+");
+    break;
+  case 80:
+    console.log("Grade: A");
+    break;
+  default:
+    console.log("Grade: B or below");
+}
+
+let day = 2; // Assuming 2 represents Tuesday
+
+switch (day) {
+  case 1:
+    console.log("monday");
+    break;
+  case 2:
+    console.log("tuesday");
+    break;
+  default:
+    console.log("invalid day");
+}
+
+/*
+  --------------------------------------------------------------
+  4. TRUTHY AND FALSY VALUES
+  --------------------------------------------------------------
+  In JavaScript, a value is either "truthy" or "falsy".
+
+  Falsy Values: 
+  - false, 0, -0, 0n (BigInt zero), "", null, undefined, NaN
+
+  Truthy Values:
+  - Everything else is truthy! (e.g., "0", "false", [], {}, 1)
+
+  --------------------------------------------------------------
+  5. LOGICAL OPERATORS IN CONDITIONALS
+  --------------------------------------------------------------
+  - && (Logical AND) : True if both operands are true.
+  - || (Logical OR)  : True if at least one operand is true.
+  - !  (Logical NOT) : Inverts the boolean value.
+*/
+
+let hasId = true;
+let isCitizen = true;
+
+if (hasId && isCitizen && age > 18) {
+  console.log("Eligible for national voting");
+}
--- javascript/scope.js
+++ javascript/scope.js
@@ -1,40 +1,101 @@
-// let a = 10;
+/*
+  ==============================================================
+  JAVASCRIPT SCOPE
+  ==============================================================
 
-//block scope
-// {
-//   const a = 10;
-//   //   console.log(a);
-// }
-// console.log(a);
+  Scope determines the accessibility (visibility) of variables.
+  In JavaScript, there are three main types of scope:
+  1. Global Scope
+  2. Function Scope
+  3. Block Scope
 
-// function abc() {
-//   var a = 10;
-//   //   console.log(a);
-// }
-// // abc();
-// console.log(a);
-// let a = 10;
-// function outer() {
-//   // console.log(a);
+  --------------------------------------------------------------
+  1. GLOBAL SCOPE
+  --------------------------------------------------------------
+  - Variables declared outside any function or block have Global Scope.
+  - They can be accessed from anywhere in the JavaScript program.
+*/
 
-//   let a = 10;
+let globalVar = 10;
 
-//   function inner() {
-//     console.log(a);
-//     // let a = 10;
-//   }
-//   inner();
-// }
+/*
+  --------------------------------------------------------------
+  2. BLOCK SCOPE
+  --------------------------------------------------------------
+  - Variables declared inside a { } block cannot be accessed from outside the block.
+  - This applies ONLY to 'let' and 'const'. 
+  - 'var' DOES NOT have block scope.
+*/
+
+{
+  const blockScoped = 10;
+  // console.log(blockScoped); // Accessible here
+}
+// console.log(blockScoped); // ReferenceError: blockScoped is not defined
+
+/*
+  --------------------------------------------------------------
+  3. FUNCTION SCOPE (Local Scope)
+  --------------------------------------------------------------
+  - Variables defined inside a function are not accessible from outside the function.
+  - This applies for 'var', 'let', and 'const'.
+*/
+
+function localScopeDemo() {
+  var functionScoped = 10;
+  // console.log(functionScoped); // Accessible here
+}
+// localScopeDemo();
+// console.log(functionScoped); // ReferenceError: functionScoped is not defined
+
+/*
+  --------------------------------------------------------------
+  4. LEXICAL SCOPE & SCOPE CHAIN
+  --------------------------------------------------------------
+  - Lexical Scope: An inner function has access to the variables 
+    of its outer function.
+  - Scope Chain: If a variable is not found in the current scope, 
+    JS looks for it in the outer scope, and so on, until the 
+    global scope.
+*/
+
+let a = 10;
+
+function outer() {
+  // console.log(a); // Accesses global 'a'
+
+  let b = 20;
+
+  function inner() {
+    console.log(a); // Accesses global 'a'
+    console.log(b); // Accesses 'b' from outer()'s scope (Lexical Scope)
+  }
+  inner();
+}
 // outer();
 
-// function outer() {
-//  let a = 10;
+/*
+  --------------------------------------------------------------
+  5. CLOSURES
+  --------------------------------------------------------------
+  - A closure is the combination of a function bundled together 
+    with references to its surrounding state (the lexical environment).
+  - It allows an inner function to access its outer scope even 
+    after the outer function has finished executing.
+*/
 
-//   function inner() {
-//     console.log(a);
-//   }
-//   return inner;
-// }
-// // let res = outer();
-// // res();
-// console.log(a);
+function closureExample() {
+  let count = 0;
+
+  function inner() {
+    count++;
+    console.log(count);
+  }
+  return inner;
+}
+
+// let res = closureExample();
+// res(); // 1
+// res(); // 2
+
+// console.log(a); // Refers to global 'a'
