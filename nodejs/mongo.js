52894bb07efe5ea33345af3465e61175776c425f
vikasthakurr <vikaskumar20012001@gmail.com>
2026-02-05 13:25:41 +0530

    crud operation

--- nodejs/mongo.js
+++ nodejs/mongo.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const PORT = 3000;
 
@@ -21,21 +22,40 @@ mongoose
 const UserSchema = new mongoose.Schema({
   email: String,
   username: String,
-  password: String
+  password: String,
 });
 
 const User = mongoose.model("User", UserSchema);
 
 app.post("/register", async (req, res) => {
-  const { email, username, password } = req.body;
+  //   const { email, username, password } = req.body;
+  let email = req.body.email;
+  let username = req.body.username;
+  let password = req.body.password;
 
-  const newUser = new User(email, username, password);
+  let hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new User({
+    email: email,
+    username: username,
+    password: hashedPassword,
+  });
 
   const savedUser = await newUser.save();
 
   if (!savedUser) return res.status(400).send({ message: err.message });
 
   res.status(201).send({ message: "user created successfully" });
+});
+
+//read
+
+app.get("/allusers", async (req, res) => {
+  const users = await User.find();
+
+  if (!users) return res.status(404).send({ message: err.message });
+
+  res.status(200).send(users);
 });
 app.get("/", (req, res) => {
   res.end("hii");
